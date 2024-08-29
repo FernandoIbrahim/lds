6c17446798c4f7e3a381961cb3b115eaddb3de0c
Fernando Antônio Ferreira Ibrahim <offut14@gmail.com>
2024-08-29 00:55:33 -0300

    feat: connection test

--- lab-01/code/app/SchoolManagementSystem/src/test/java/com/example/SchoolManagementSystem/SchoolManagementSystemApplicationTests.java
+++ lab-01/code/app/SchoolManagementSystem/src/test/java/com/example/SchoolManagementSystem/SchoolManagementSystemApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.SchoolManagementSystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class SchoolManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @Test
+    public void testDatabaseConnection() {
+        // Teste básico para verificar se a conexão está funcionando
+        Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        assertThat(result).isEqualTo(1);
+    }
 }
