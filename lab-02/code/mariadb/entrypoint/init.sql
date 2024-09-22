18fb06b3c9fa66df8f2b41fa957e234987a76f7e
Jhonata S. Dias <118060258+jhonatasdias@users.noreply.github.com>
2024-09-22 10:56:46 -0300

    fix: correção no arquivo init.sql

--- lab-02/code/mariadb/entrypoint/init.sql
+++ lab-02/code/mariadb/entrypoint/init.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS pessoa_fisica (
     id_usuario INT NOT NULL,
     nome VARCHAR(100) NOT NULL,
     rg VARCHAR(15) NOT NULL UNIQUE,
-    cpf VARCHAR(15) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
     profissao VARCHAR(255),
     empregadora VARCHAR(255),
     PRIMARY KEY (id),
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS pessoa_juridica (
     id INT AUTO_INCREMENT NOT NULL,
     id_usuario INT NOT NULL,
     nome_fantasia VARCHAR(100) NOT NULL,
-    cnpj VARCHAR(15) NOT NULL,
+    cnpj VARCHAR(14) NOT NULL UNIQUE,
     PRIMARY KEY (id),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
 );
