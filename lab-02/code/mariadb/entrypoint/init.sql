024035d5ef6236391ffcf8be4997eca95b5e653c
Fernando Antônio Ferreira Ibrahim <80763509+FernandoIbrahim@users.noreply.github.com>
2024-09-24 10:57:46 -0300

    criação de tabelas

--- lab-02/code/mariadb/entrypoint/init.sql
+++ lab-02/code/mariadb/entrypoint/init.sql
@@ -30,3 +30,45 @@ CREATE TABLE IF NOT EXISTS pessoa_juridica (
     PRIMARY KEY (id),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
 );
+
+-- Tabela automoveis com chave estrangeira para usuario
+CREATE TABLE IF NOT EXISTS automovel (
+	matricula INT NOT NULL,
+	ano INT NOT NULL,
+	marca VARCHAR(100) NOT NULL,
+	modelo VARCHAR(100) NOT NULL,
+	placa VARCHAR(7) NOT NULL,
+	id_usuario INT NOT NULL,
+	PRIMARY KEY (matricula),
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+
+
+-- Tabela pedidos de aluguel com chave estrangeira para usuario
+CREATE TABLE IF NOT EXISTS pedido_aluguel (
+	id INT AUTO_INCREMENT NOT NULL,
+	aprovacao BOOL NOT NULL,
+	total DECIMAL NOT NULL,
+	data_inicio DATE NOT NULL,
+	data_fim DATE NOT NULL,
+	id_cliente INT NOT NULL,
+	matricula_automovel INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_cliente) REFERENCES pessoa_fisica(id) ON DELETE CASCADE,
+	FOREIGN KEY (matricula_automovel) REFERENCES automovel(matricula) ON DELETE CASCADE
+);
+
+
+
+
+-- Tabela  com chave estrangeira para usuario
+CREATE TABLE IF NOT EXISTS rendimentos (
+	id INT NOT NULL,
+	provento DECIMAL NOT NULL,
+	data DATE NOT NULL,
+	id_usuario INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
